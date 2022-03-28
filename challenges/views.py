78f35868bc24265e4d0e71b4acadc2d3dc7a7fb1
Alex Labanino <ghostrider6610@gmail.com>
2022-03-27 22:21:22 -0400

    Returning HTML

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from turtle import forward
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     'january': 'Go to the gym!',
@@ -25,12 +26,14 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound('Invalid month')
 
     redirect_month = months[month -1]
-    return HttpResponseRedirect('/challenges/' + redirect_month)
+    redirect_path = reverse('month-challenge', args = [redirect_month]) # /challenge/january
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
+        response_data = f'<h1>{challenge_text}</h1>'
         return HttpResponse(challenge_text)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
